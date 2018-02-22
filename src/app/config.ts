c2536e27e64c70cdd50f259ee33708bffa9064d3
dogfish <dyingsunlight@gmail.com>
2018-02-23 01:38:49 +0800

    【修复】【路由】 url在生产模式失效

--- src/app/config.ts
+++ src/app/config.ts
@@ -7,7 +7,7 @@ if (environment.production) {
 }
 console.log(environment.production);
 export const url = {
-  base: base,
+  base: base.slice(1),
   basePath: base + 'assets/images/',
   imageBasePath: base + 'assets/images/icons/',
   gachaBasePath: base + 'assets/images/gacha/'
