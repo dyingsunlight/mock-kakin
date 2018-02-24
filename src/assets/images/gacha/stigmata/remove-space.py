f180294bcc74d143356427dc2039e66aea95ca44
dogfish <dyingsunlight@gmail.com>
2018-02-25 01:31:39 +0800

    【添加】【原始资源】已压缩的图片资源

---
+++ src/assets/images/gacha/stigmata/remove-space.py
@@ -0,0 +1,17 @@
+import re
+import os
+
+def start():
+    print('initial function')
+    path = os.getcwd()  # 文件夹目录
+    files = os.listdir(path)  # 得到文件夹下的所有文件名称
+    s = []
+    regex = re.compile(r" .png$", re.IGNORECASE)
+    for file in files:  # 遍历文件夹
+        if not os.path.isdir(file):  # 判断是否是文件夹，不是文件夹才打开
+            original_name = file
+            print(original_name)
+            file = re.sub(regex, '.png', file)
+            os.rename(path + "/" + original_name, file)
+
+start()
