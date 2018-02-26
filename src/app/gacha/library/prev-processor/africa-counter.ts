71087c317e827126837f87543ed65cc9a176cfff
dogfish <dyingsunlight@gmail.com>
2018-02-26 18:15:24 +0800

    【重构】【非洲福音】 * 标配只能保底角色卡，装备和精准只能保底装备

--- src/app/gacha/library/prev-processor/africa-counter.ts
+++ src/app/gacha/library/prev-processor/africa-counter.ts
@@ -1,28 +1,44 @@
 import { GachaPreProcessParams } from '../../interface/gacha-pre-process';
 // 非洲人保底计数器
 import { GachaItem } from '../../interface/gacha-item';
-// 4*物品触发重置保底的类别
-const resetList = ['stigmata', 'weapon', 'character'];
 // 下一次保底剩余
 const resetProtection: {[type: string]: number} = {};
-
 export const africaCounterProcessor = function(params: GachaPreProcessParams): GachaItem {
   const item: GachaItem = params.item;
   const controlParams = params.controlParams;
-  const isNeedReset = resetList.includes(item.type) && item.level === 4;
   if (!resetProtection.hasOwnProperty(controlParams.mode)) {
     resetProtection[controlParams.mode] = 1;
   }
-  // 一直抽取，直到保底位置，非洲人可能会死机
-  if (resetProtection[controlParams.mode] === 10 && !isNeedReset) {
-    // console.log('强制保底');
-    // todo 加个强制抽取N次后取得物品的通知
-    return;
+  if (resetProtection[controlParams.mode] >= 10) {
+    if (controlParams.mode === 'standard' && item.level < 12) {
+      // 只保底角色卡
+      return;
+    }
+    if (controlParams.mode === 'precision' && item.level !== 4) {
+      // 只保底装备
+      return;
+    }
+    if (controlParams.mode === 'equipment' && item.level !== 4) {
+      // 只保底装备
+      return;
+    }
   }
-  if (isNeedReset) {
-    resetProtection[controlParams.mode] = 1;
-  } else {
-    resetProtection[controlParams.mode] ++;
+  if (resetProtection[controlParams.mode] < 10) {
+    let isNeedReset = false;
+    if (controlParams.mode === 'standard' && item.level > 11) {
+      isNeedReset = true;
+    }
+    if (controlParams.mode === 'precision' && item.level === 4) {
+      isNeedReset = true;
+    }
+    if (controlParams.mode === 'equipment' && item.level === 4) {
+      isNeedReset = true;
+    }
+    if (isNeedReset) {
+      resetProtection[controlParams.mode] = 1;
+    } else {
+      resetProtection[controlParams.mode]++;
+    }
   }
   return item;
 };
