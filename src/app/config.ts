8f049ad14cee327fd57155b8c5156f1b28d624e5
dogfish <dyingsunlight@gmail.com>
2018-03-09 00:16:37 +0800

    【修复】基础链接

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,12 +1,12 @@
 import { environment } from '../environments/environment';
 
-const images = environment.resourcesPath + '/assets/images/';
+const images = environment.resourcesPath + 'assets/images/';
 export const url = {
   base: environment.resourcesPath,
   images: images,
-  backgrounds: images + '/backgrounds/',
-  icons: images + '/icons/',
-  gacha: images + '/gacha/'
+  backgrounds: images + 'backgrounds/',
+  icons: images + 'icons/',
+  gacha: images + 'gacha/'
 };
 
 
--- src/environments/environment.github-pages.prod.ts
+++ src/environments/environment.github-pages.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  resourcesPath: ''
+  resourcesPath: '/mock-kakin/'
 };
