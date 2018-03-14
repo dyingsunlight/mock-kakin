eac13c753f87750222b298f69b233fc6a86b4fdf
dogfish <dyingsunlight@gmail.com>
2018-03-14 22:21:50 +0800

    【修复】【issue #4】修复了概率测试中，角色卡和碎片名称冲突，导致角色卡显示不全的问题。

--- src/app/gacha/library/prev-processor/adapter/adapter-name.ts
+++ src/app/gacha/library/prev-processor/adapter/adapter-name.ts
@@ -2,19 +2,25 @@ import { GachaPreProcessParams } from '../../../interface/gacha-pre-process';
 import { stigmataSlot } from '../../../enum/stigmata-slot';
 
 const stigmataSlotName = Object.keys(stigmataSlot).map(key => stigmataSlot[key]);
-const stigmataSlotFileName = ['_1', '_2', '_3'];
-let count = 0;
+
 export const AdapterForName = function(params: GachaPreProcessParams) {
   const item = params.item;
   item.extra = {};
-  if (item.type === 'stigmata') {
-    if (stigmataSlotName.includes(item.name.slice(-3))) {
-      item.extra.suffix = item.name.slice(-3);
-      item.name = item.name.slice(0, -3);
-    }
-  } else {
+  switch (item.type) {
+    case 'stigmata':
+      if (stigmataSlotName.includes(item.name.slice(-3))) {
+        item.extra.suffix = item.name.slice(-3);
+        item.name = item.name.slice(0, -3);
+      }
+      break;
+    case 'fragment':
+      item.extra.suffix = '[碎片]';
+      break;
+    default:
     item.extra.suffix = '';
+    break;
   }
+
   return item;
 };
 
